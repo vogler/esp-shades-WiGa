93ceabb9814b3d6b3e1bc349f212ca7d6b7357a3
Ralf Vogler <ralf.vogler@gmail.com>
2020-12-26 19:44:17 +0100

    decided on pins, keep RX/TX for serial for now

--- esp-shades-WiGa.ino
+++ esp-shades-WiGa.ino
@@ -13,21 +13,27 @@ void setup() {
   setup_WiFi();
   setup_OTA();
 
+  // Need 6 output and 6 input pins, but there are only 9 GPIO on Wemos D1 mini.
+  // -> Use RX/TX as GPIO -> no more serial after setup.
+  // -> Use A0 as input. Goes low over time -> pull high.
+  // D8 is pulled down -> pair with A0.
+  // Other pins (except D0) are high with INPUT_PULLUP.
+
   // output: 6 relays, 2 for each motor: 1. power on/off, 2. up/down
   // change the UART pins to GPIO, https://arduino.stackexchange.com/questions/29938/how-to-i-make-the-tx-and-rx-pins-on-an-esp-8266-01-into-gpio-pins
   // won't be able to use serial monitor after this, change back with FUNCTION_0
   //pinMode(1, FUNCTION_3); // TX
   //pinMode(3, FUNCTION_3); // RX
   // int output_pins[6] = { D3, D4, TX, RX, D0, D8 };
-//  int output_pins[] = { D3, D4, D0, D8 };
-//  for (int i = 0; i < 4; i++) {
-//    pinMode(output_pins[i], OUTPUT);
-//    digitalWrite(output_pins[i], HIGH);
-//  }
+  int output_pins[] = { D4, D3, D8, D0 }; // RX, TX
+  for (int i = 0; i < 4; i++) {
+    pinMode(output_pins[i], OUTPUT);
+    digitalWrite(output_pins[i], HIGH);
+  }
 
   // input: 6 pins for 3 up/down switches
-  pinMode(D0, INPUT_PULLUP); // 0 floating
-  pinMode(D8, INPUT_PULLUP); // 0 pull-down
+  // pinMode(D0, INPUT_PULLUP); // 0 floating
+  // pinMode(D8, INPUT_PULLUP); // 0 pull-down
   pinMode(D1, INPUT_PULLUP); // 1
   pinMode(D2, INPUT_PULLUP); // 1
   pinMode(D5, INPUT_PULLUP); // 1
@@ -45,21 +51,12 @@ struct motor m3 = { RX, TX };
 void loop() {
   ArduinoOTA.handle();
   
-  printRead(D0);
   printRead(D1);
   printRead(D2);
   printRead(D5);
   printRead(D6);
   printRead(D7);
-  printRead(D8);
   Serial.printf("A0: %u\n", analogRead(A0));
   
   delay(500);
-
-//  delay(5000);
-//
-//  power(m1, ON);
-//  delay(1000);
-//  power(m1, OFF);
-//  delay(1000);
 }
